1673602a7100a9f78feaca27b906a2e009aa72d7
jagpreetsinghsaan <jagpreet.singh.sasan@accenture.com>
2021-08-02 12:37:52 +0530

    GG commit
    Signed-off-by: jagpreetsinghsaan <jagpreet.singh.sasan@accenture.com>

---
+++ extensions/cactus-plugin-htlc-coordinator/src/test/typescript/integration/plugin-htlc-coordinator/own-htlc-express.test.ts
@@ -0,0 +1,402 @@
+// import test, { Test } from "tape-promise/tape";
+// import { v4 as uuidv4 } from "uuid";
+// import { createServer } from "http";
+// import { AddressInfo } from "net";
+// import express from "express";
+// import bodyParser from "body-parser";
+// import http from "http";
+// import { Server as SocketIoServer } from "socket.io";
+// import {
+//   DefaultApi as HtlcCoordinatorApi,
+//   PluginFactoryHTLCCoordinator,
+//   IPluginHTLCCoordinatorOptions,
+//   HtlcPackage,
+//   OwnHTLCRequest,
+//   CounterpartyHTLCRequest,
+//   WithdrawCounterpartyRequest,
+//   Configuration,
+// } from "../../../../main/typescript/public-api";
+// import {
+//   DefaultApi as BesuApi,
+//   EthContractInvocationType,
+//   PluginFactoryLedgerConnector,
+//   PluginLedgerConnectorBesu,
+//   Web3SigningCredentialType,
+//   Web3SigningCredential,
+// } from "@hyperledger/cactus-plugin-ledger-connector-besu";
+// import {
+//   LogLevelDesc,
+//   IListenOptions,
+//   Servers,
+// } from "@hyperledger/cactus-common";
+// import { PluginRegistry } from "@hyperledger/cactus-core";
+// import { Constants, PluginImportType } from "@hyperledger/cactus-core-api";
+// import {
+//   BesuTestLedger,
+//   pruneDockerAllIfGithubAction,
+// } from "@hyperledger/cactus-test-tooling";
+// import { PluginKeychainMemory } from "@hyperledger/cactus-plugin-keychain-memory";
+
+// import HashTimeLockJSON from "@hyperledger/cactus-plugin-htlc-eth-besu-erc20/src/main/solidity/contracts/HashedTimeLockContract.json";
+// import TestTokenJSON from "../../../../../../../packages/cactus-test-plugin-htlc-eth-besu-erc20/src/test/solidity/token-erc20-contract/Test_Token.json";
+// import DemoHelperJSON from "../../../../../../../packages/cactus-test-plugin-htlc-eth-besu-erc20/src/test/solidity/token-erc20-contract/DemoHelpers.json";
+
+// const logLevel: LogLevelDesc = "INFO";
+// const estimatedGas = 6721975;
+// const expiration = 2147483648;
+// const secret =
+//   "0x3853485acd2bfc3c632026ee365279743af107a30492e3ceaa7aefc30c2a048a";
+// const hashLock =
+//   "0x3c335ba7f06a8b01d0596589f73c19069e21c81e5013b91f408165d1bf623d32";
+// const firstHighNetWorthAccount = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1";
+// const privateKey =
+//   "0x4f3edf983ac636a65a842ce7c78d9aa706d3b113bce9c46f30d7d21715b23b1d";
+// const connectorInstanceId = uuidv4();
+// const web3SigningCredential: Web3SigningCredential = {
+//   ethAccount: firstHighNetWorthAccount,
+//   secret: privateKey,
+//   type: Web3SigningCredentialType.PrivateKeyHex,
+// } as Web3SigningCredential;
+// const contractAddress = "0xCfEB869F69431e42cdB54A4F4f105C19C080A601";
+
+// const testCase = "Test own htlc endpoint";
+
+// test("BEFORE " + testCase, async (t: Test) => {
+//   const pruning = pruneDockerAllIfGithubAction({ logLevel });
+//   await t.doesNotReject(pruning, "Pruning did not throw OK");
+//   t.end();
+// });
+
+// test(testCase, async (t: Test) => {
+//   t.comment("Starting Besu Test Ledger");
+//   const besuTestLedger = new BesuTestLedger();
+//   await besuTestLedger.start();
+
+//   test.onFinish(async () => {
+//     await besuTestLedger.stop();
+//     await besuTestLedger.destroy();
+//   });
+
+//   const rpcApiHttpHost = await besuTestLedger.getRpcApiHttpHost();
+//   const rpcApiWsHost = await besuTestLedger.getRpcApiWsHost();
+//   const keychainId = uuidv4();
+//   const keychainPlugin = new PluginKeychainMemory({
+//     instanceId: uuidv4(),
+//     keychainId,
+//     // pre-provision keychain with mock backend holding the private key of the
+//     // test account that we'll reference while sending requests with the
+//     // signing credential pointing to this keychain entry.
+//     backend: new Map([[TestTokenJSON.contractName, TestTokenJSON]]),
+//     logLevel,
+//   });
+//   keychainPlugin.set(DemoHelperJSON.contractName, DemoHelperJSON);
+//   keychainPlugin.set(HashTimeLockJSON.contractName, HashTimeLockJSON);
+
+//   const receiverAccount = await besuTestLedger.createEthTestAccount();
+
+//   const web3SigningCredentialReceiverAcc: Web3SigningCredential = {
+//     ethAccount: receiverAccount.address,
+//     secret: receiverAccount.privateKey,
+//     type: Web3SigningCredentialType.PrivateKeyHex,
+//   } as Web3SigningCredential;
+
+//   const factory = new PluginFactoryLedgerConnector({
+//     pluginImportType: PluginImportType.Local,
+//   });
+
+//   const pluginRegistry = new PluginRegistry({});
+//   const connector: PluginLedgerConnectorBesu = await factory.create({
+//     rpcApiHttpHost,
+//     rpcApiWsHost,
+//     logLevel,
+//     instanceId: connectorInstanceId,
+//     pluginRegistry: new PluginRegistry({ plugins: [keychainPlugin] }),
+//   });
+
+//   pluginRegistry.add(connector);
+//   const pluginOptions: IPluginHTLCCoordinatorOptions = {
+//     instanceId: uuidv4(),
+//     logLevel,
+//     pluginRegistry,
+//   };
+
+//   const factoryHTLC = new PluginFactoryHTLCCoordinator({
+//     pluginImportType: PluginImportType.Local,
+//   });
+//   const pluginHTLCCoordinator = await factoryHTLC.create(pluginOptions);
+//   pluginRegistry.add(pluginHTLCCoordinator);
+
+//   const expressApp = express();
+//   expressApp.use(bodyParser.json({ limit: "250mb" }));
+//   const server = http.createServer(expressApp);
+//   const listenOptions: IListenOptions = {
+//     hostname: "0.0.0.0",
+//     port: 0,
+//     server,
+//   };
+//   const addressInfo = (await Servers.listen(listenOptions)) as AddressInfo;
+//   test.onFinish(async () => await Servers.shutdown(server));
+//   const { address, port } = addressInfo;
+//   const apiHost = `http://${address}:${port}`;
+
+//   const configuration = new Configuration({ basePath: apiHost });
+//   const htlcCoordinatorApi = new HtlcCoordinatorApi(configuration);
+
+//   await pluginHTLCCoordinator.getOrCreateWebServices();
+//   await pluginHTLCCoordinator.registerWebServices(expressApp);
+//   const besuWsApi = new SocketIoServer(server, {
+//     path: Constants.SocketIoConnectionPathV1,
+//   });
+//   const besuConnectorConfiguration = new Configuration({ basePath: apiHost });
+//   const besuConnectorApi = new BesuApi(besuConnectorConfiguration);
+
+//   await connector.getOrCreateWebServices();
+//   await connector.registerWebServices(expressApp, besuWsApi as any)
+
+//   /**
+//    *
+//    *
+//    *
+//    * For firstHighNetworthAccount
+//    *
+//    *
+//    *
+//    *
+//    *
+//    */
+
+//   t.comment("Deploys TestToken via .json file on deployContract function");
+//   const deployOutTokenSenderAccount = await connector.deployContract({
+//     contractName: TestTokenJSON.contractName,
+//     contractAbi: TestTokenJSON.abi,
+//     bytecode: TestTokenJSON.bytecode,
+//     web3SigningCredential,
+//     keychainId,
+//     constructorArgs: ["100", "token", "2", "TKN"],
+//     gas: estimatedGas,
+//   });
+//   t.ok(deployOutTokenSenderAccount, "deployContract() output is truthy OK");
+//   t.ok(
+//     deployOutTokenSenderAccount.transactionReceipt,
+//     "deployContract() output.transactionReceipt is truthy OK",
+//   );
+//   t.ok(
+//     deployOutTokenSenderAccount.transactionReceipt.contractAddress,
+//     "deployContract() output.transactionReceipt.contractAddress is truthy OK",
+//   );
+//   const tokenAddressSenderAccount = deployOutTokenSenderAccount.transactionReceipt
+//     .contractAddress as string;
+
+//   t.comment("Approve 10 Tokens to HashTimeLockAddress");
+//   const approveTokensOutputSenderAccount = await besuConnectorApi.apiV1BesuInvokeContract({
+//     contractName: TestTokenJSON.contractName,
+//     keychainId,
+//     signingCredential: web3SigningCredential,
+//     invocationType: EthContractInvocationType.Send,
+//     methodName: "approve",
+//     params: [contractAddress, "10"],
+//     gas: estimatedGas,
+//   });
+//   t.equal(approveTokensOutputSenderAccount.data.success, true, "approve() transactionReceipt.status is true OK");
+
+//   t.comment("Get account balance");
+//   const responseBalanceSenderAccount = await besuConnectorApi.apiV1BesuInvokeContract({
+//     contractName: TestTokenJSON.contractName,
+//     keychainId,
+//     signingCredential: web3SigningCredential,
+//     invocationType: EthContractInvocationType.Call,
+//     methodName: "balanceOf",
+//     params: [firstHighNetWorthAccount],
+//   });
+//   t.equal(responseBalanceSenderAccount.data.callOutput, "100", "balance of account is 100 OK");
+
+//   t.comment("Get HashTimeLock contract and account allowance");
+//   const allowanceOutput1 = await besuConnectorApi.apiV1BesuInvokeContract({
+//     contractName: TestTokenJSON.contractName,
+//     keychainId,
+//     signingCredential: web3SigningCredential,
+//     invocationType: EthContractInvocationType.Call,
+//     methodName: "allowance",
+//     params: [firstHighNetWorthAccount, contractAddress],
+//   });
+//   t.equal(allowanceOutput1.data.callOutput, "10", "callOutput() is 10 OK");
+
+//   /**
+//    *
+//    *
+//    *
+//    *
+//    * For receiver account
+//    *
+//    *
+//    *
+//    *
+//    */
+
+//   t.comment("Deploys TestToken via .json file on deployContract function");
+//   const deployOutTokenReceiverAccount = await connector.deployContract({
+//     contractName: TestTokenJSON.contractName,
+//     contractAbi: TestTokenJSON.abi,
+//     bytecode: TestTokenJSON.bytecode,
+//     web3SigningCredential: web3SigningCredentialReceiverAcc,
+//     keychainId,
+//     constructorArgs: ["50", "token", "2", "TKN"],
+//     gas: estimatedGas,
+//   });
+//   t.ok(deployOutTokenReceiverAccount, "deployContract() output is truthy OK");
+//   t.ok(
+//     deployOutTokenReceiverAccount.transactionReceipt,
+//     "deployContract() output.transactionReceipt is truthy OK",
+//   );
+//   t.ok(
+//     deployOutTokenReceiverAccount.transactionReceipt.contractAddress,
+//     "deployContract() output.transactionReceipt.contractAddress is truthy OK",
+//   );
+//   const tokenAddressReceiverAccount = deployOutTokenReceiverAccount.transactionReceipt
+//     .contractAddress as string;
+
+//   t.comment("Approve 10 Tokens to HashTimeLockAddress");
+//   const approveTokensOutputReceiverAccount = await besuConnectorApi.apiV1BesuInvokeContract({
+//     contractName: TestTokenJSON.contractName,
+//     keychainId,
+//     signingCredential: web3SigningCredentialReceiverAcc,
+//     invocationType: EthContractInvocationType.Send,
+//     methodName: "approve",
+//     params: [contractAddress, "10"],
+//     gas: estimatedGas,
+//   });
+//   t.equal(approveTokensOutputReceiverAccount.data.success, true, "approve() transactionReceipt.status is true OK");
+
+//   t.comment("Get account balance");
+//   const responseBalanceReceiverAccount = await besuConnectorApi.apiV1BesuInvokeContract({
+//     contractName: TestTokenJSON.contractName,
+//     keychainId,
+//     signingCredential: web3SigningCredentialReceiverAcc,
+//     invocationType: EthContractInvocationType.Call,
+//     methodName: "balanceOf",
+//     params: [receiverAccount.address],
+//   });
+//   t.equal(responseBalanceReceiverAccount.data.callOutput, "50", "balance of account is 50 OK");
+
+//   t.comment("Get HashTimeLock contract and account allowance");
+//   const allowanceOutput2 = await besuConnectorApi.apiV1BesuInvokeContract({
+//     contractName: TestTokenJSON.contractName,
+//     keychainId,
+//     signingCredential: web3SigningCredentialReceiverAcc,
+//     invocationType: EthContractInvocationType.Call,
+//     methodName: "allowance",
+//     params: [receiverAccount.address, contractAddress],
+//   });
+//   t.equal(allowanceOutput2.data.callOutput, "10", "callOutput() is 10 OKkkkkkkkkkkkkkkkkkkkk");
+
+//   /**
+//    *
+//    *
+//    * HELP!!!
+//    *
+//    *
+//    */
+//    t.comment("Get HashTimeLock contract and account allowance");
+//    const allowanceOutput3 = await besuConnectorApi.apiV1BesuInvokeContract({
+//      contractName: TestTokenJSON.contractName,
+//      keychainId,
+//      signingCredential: web3SigningCredential,
+//      invocationType: EthContractInvocationType.Call,
+//      methodName: "allowance",
+//      params: [firstHighNetWorthAccount, contractAddress],
+//    });
+//    t.equal(allowanceOutput3.data.callOutput, "10", "callOutput() is 10 OK");
+
+//   // t.comment("Create and initialize own HTLC");
+//   // const ownHTLCRequest: OwnHTLCRequest = {
+//   //   htlcPackage: HtlcPackage.BesuErc20,
+//   //   connectorInstanceId,
+//   //   keychainId,
+//   //   constructorArgs: [],
+//   //   web3SigningCredential,
+//   //   inputAmount: 10,
+//   //   outputAmount: 1,
+//   //   expiration,
+//   //   hashLock,
+//   //   tokenAddress: tokenAddressSenderAccount,
+//   //   receiver: receiverAccount.address,
+//   //   outputNetwork: "BTC",
+//   //   outputAddress: "1AcVYm7M3kkJQH28FXAvyBFQzFRL6xPKu8",
+//   //   gas: estimatedGas,
+//   // };
+
+//   // const response = await htlcCoordinatorApi.ownHtlcV1(ownHTLCRequest);
+//   // t.equal(response.status, 200, "response status is 200 OK");
+//   // t.equal(response.data.success, true, "response success is true");
+//   // t.ok(response.data, "pluginHTLCCoordinator.ownHtlcV1() output is truthy OK");
+//   // t.ok(
+//   //   response.data.out.transactionReceipt,
+//   //   "pluginHTLCCoordinator.ownHtlcV1() output.transactionReceipt is truthy OK",
+//   // );
+
+//   // t.comment("Get HTLC id");
+//   // const responseTxId = await besuConnectorApi.apiV1BesuInvokeContract({
+//   //   contractName: DemoHelperJSON.contractName,
+//   //   keychainId,
+//   //   signingCredential: web3SigningCredential,
+//   //   invocationType: EthContractInvocationType.Call,
+//   //   methodName: "getTxId",
+//   //   params: [
+//   //     firstHighNetWorthAccount,
+//   //     receiverAccount.address,
+//   //     10,
+//   //     hashLock,
+//   //     expiration,
+//   //     tokenAddress,
+//   //   ],
+//   //   gas: estimatedGas,
+//   // });
+
+//   // t.comment("Get counterparty HTLC");
+//   // const counterpartyHTLCRequest: CounterpartyHTLCRequest = {
+//   //   htlcPackage: HtlcPackage.BesuErc20,
+//   //   connectorInstanceId,
+//   //   keychainId,
+//   //   htlcId: responseTxId.data.callOutput,
+//   //   web3SigningCredential,
+//   //   gas: estimatedGas,
+//   // };
+
+//   // const response2 = await htlcCoordinatorApi.counterpartyHtlcV1(counterpartyHTLCRequest);
+//   // t.equal(response2.status, 200, "response status is 200 OK");
+//   // t.equal(response2.data.success, true, "response success is true");
+//   // t.equal(response2.data.callOutput, "1", "the contract status is 1 - Active");
+
+//   // t.comment("Get counterparty HTLC");
+//   // const withdrawCounterparty: WithdrawCounterpartyRequest = {
+//   //   htlcPackage: HtlcPackage.BesuErc20,
+//   //   connectorInstanceId,
+//   //   keychainId,
+//   //   web3SigningCredential,
+//   //   htlcId: responseTxId.data.callOutput,
+//   //   secret,
+//   //   gas: estimatedGas,
+//   // };
+
+//   // const response3 = await htlcCoordinatorApi.withdrawCounterpartyV1(withdrawCounterparty);
+//   // t.equal(response3.status, 200, "response status is 200 OK");
+//   // t.equal(response3.data.success, true, "response success is true");
+
+//   // t.comment("Get balance of receiver account");
+//   // const responseFinalBalance = await connector.invokeContract({
+//   //   contractName: TestTokenJSON.contractName,
+//   //   keychainId,
+//   //   signingCredential: web3SigningCredential,
+//   //   invocationType: EthContractInvocationType.Call,
+//   //   methodName: "balanceOf",
+//   //   params: [receiverAccount.address],
+//   // });
+//   // t.equal(responseFinalBalance.callOutput, "10", "balance of account is 10 OK");
+//   // t.end();
+// });
+
+// test("AFTER " + testCase, async (t: Test) => {
+//   const pruning = pruneDockerAllIfGithubAction({ logLevel });
+//   await t.doesNotReject(pruning, "Pruning did not throw OK");
+//   t.end();
+// });
