5dca1b10de3e2d87dfbe1679799259d49aff5b62
jagpreetsinghsasan <jagpreet.singh.sasan@accenture.com>
2024-04-03 11:14:14 +0530

    Mods patched
    Signed-off-by: jagpreetsinghsasan <jagpreet.singh.sasan@accenture.com>

--- examples/bevel/tpls/k8ind.go
+++ examples/bevel/tpls/k8ind.go
@@ -7,11 +7,11 @@ nodes:
 {{ $IncrementedControlPaneCount := .ControlPlaneCount | add1 | int }}
 {{ range $i, $e := untilStep 1 $IncrementedControlPaneCount 1 }}
 - role: control-plane
-  image: kindest/node:v1.27.11@sha256:681253009e68069b8e01aad36a1e0fa8cf18bb0ab3e5c4069b2e65cafdd70843
+  image: kindest/node:v1.26.14@sha256:5d548739ddef37b9318c70cb977f57bf3e5015e4552be4e27e57280a8cbb8e4f
 {{ end }}
 {{ $IncrementedWorkerCount := .WorkerNodeCount | add1 | int }}
 {{ range $i, $e := untilStep 1 $IncrementedWorkerCount 1 }}
 - role: worker
-  image: kindest/node:v1.27.11@sha256:681253009e68069b8e01aad36a1e0fa8cf18bb0ab3e5c4069b2e65cafdd70843
+  image: kindest/node:v1.26.14@sha256:5d548739ddef37b9318c70cb977f57bf3e5015e4552be4e27e57280a8cbb8e4f
 {{ end }}
 `
