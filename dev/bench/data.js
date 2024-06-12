c72121f7355217910667cc531b9d47a42f05f4fc
github-action-benchmark <github@users.noreply.github.com>
2024-06-12 03:32:22 +0000

    add Benchmark (benchmarkjs) benchmark result for cc2f9c58975f72907576c529c2094b330d8b2b92

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1718162147992,
+  "lastUpdate": 1718163142561,
   "repoUrl": "https://github.com/jagpreetsinghsasan/cactus",
   "entries": {
     "Benchmark": [
@@ -36,6 +36,37 @@ window.BENCHMARK_DATA = {
             "name": "cmd-api-server_gRPC_GetOpenApiSpecV1",
             "value": 367,
             "range": "±1.70%",
+            "unit": "ops/sec",
+            "extra": "181 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "vramakr2@in.ibm.com",
+            "name": "VRamakrishna",
+            "username": "VRamakrishna"
+          },
+          "committer": {
+            "email": "sandeepn.official@gmail.com",
+            "name": "Sandeep Nishad",
+            "username": "sandeepnRES"
+          },
+          "distinct": true,
+          "id": "cc2f9c58975f72907576c529c2094b330d8b2b92",
+          "message": "ci(ossf): configured GitHub action and badge for OpenSSF scorecard\n\nSigned-off-by: VRamakrishna <vramakr2@in.ibm.com>",
+          "timestamp": "2024-06-10T01:11:43+05:30",
+          "tree_id": "8f80768252cab6792094c2872aebb5438315caff",
+          "url": "https://github.com/jagpreetsinghsasan/cactus/commit/cc2f9c58975f72907576c529c2094b330d8b2b92"
+        },
+        "date": 1718163140726,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "plugin-ledger-connector-besu_HTTP_GET_getOpenApiSpecV1",
+            "value": 743,
+            "range": "±2.78%",
             "unit": "ops/sec",
             "extra": "181 samples"
           }
