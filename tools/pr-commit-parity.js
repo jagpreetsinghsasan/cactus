419456e59848e03b6be504ddce994de58789d2e9
jagpreetsinghsasan <jagpreet.singh.sasan@accenture.com>
2024-07-16 11:41:15 +0530

    TESTING
    Signed-off-by: jagpreetsinghsasan <jagpreet.singh.sasan@accenture.com>

--- tools/pr-commit-parity.js
+++ tools/pr-commit-parity.js
@@ -1,10 +1,10 @@
 export async function getCommitMessageStringArray(url) {
   const commitMessagesJsonResponse = await fetch(url);
-  return commitMessagesJsonResponse.text();
+  return commitMessagesJsonResponse.json();
 }
 
 const args = process.argv.slice(2);
 const pullReqUrl = args[0];
 
 const res = await getCommitMessageStringArray(pullReqUrl);
-console.log(res);
+console.log(res.body);
