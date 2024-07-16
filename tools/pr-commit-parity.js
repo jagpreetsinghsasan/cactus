87f28541c51e2fde2c8db1572ef93319cccd3a3e
jagpreetsinghsasan <jagpreet.singh.sasan@accenture.com>
2024-07-16 11:13:13 +0530

    TESTING
    Signed-off-by: jagpreetsinghsasan <jagpreet.singh.sasan@accenture.com>

--- tools/pr-commit-parity.js
+++ tools/pr-commit-parity.js
@@ -5,10 +5,10 @@
 
 const pullReqUrl = process.env.PULL_REQ_URL;
 const pullReqTitle = process.env.PULL_REQ_TITLE;
-const pullReqBody = process.env.PULL_REQ_BODY;
-const githubApiCommitIdsRef = pullReqBody + "/commits";
+// const pullReqBody = process.env.PULL_REQ_BODY;
+// const githubApiCommitIdsRef = pullReqBody + "/commits";
 console.log(pullReqUrl);
 console.log(pullReqTitle);
-console.log(githubApiCommitIdsRef);
+// console.log(githubApiCommitIdsRef);
 // const res = await getCommitMessageStringArray(githubApiCommitIdsRef);
 // console.log(JSON.stringify(res));
