d36934137edd3b53d50b06fbde269d7d09e52b36
jagpreetsinghsasan <jagpreet.singh.sasan@accenture.com>
2024-07-16 11:29:42 +0530

    TESTING
    Signed-off-by: jagpreetsinghsasan <jagpreet.singh.sasan@accenture.com>

--- tools/pr-commit-parity.js
+++ tools/pr-commit-parity.js
@@ -1,16 +1,16 @@
-// export async function getCommitMessageStringArray(url: string) {
-//   const commitMessagesJson = await fetch(url);
-//   return commitMessagesJson;
-// }
+export async function getCommitMessageStringArray(url: string) {
+  const commitMessagesJson = await fetch(url);
+  return commitMessagesJson;
+}
 
 const args = process.argv.slice(2);
 
 // const pullReqUrl = args[0];
-const pullReqTitle = args[1];
-// const pullReqBody = args[2];
-// const githubApiCommitIdsRef = pullReqBody + "/commits";
+// const pullReqTitle = args[1];
+const pullReqBody = args[2];
+const githubApiCommitIdsRef = pullReqBody + "/commits";
 // console.log(pullReqUrl);
-console.log(pullReqTitle);
+// console.log(pullReqTitle);
 // console.log(githubApiCommitIdsRef);
-// const res = await getCommitMessageStringArray(githubApiCommitIdsRef);
-// console.log(JSON.stringify(res));
+const res = await getCommitMessageStringArray(githubApiCommitIdsRef);
+console.log(JSON.stringify(res));
